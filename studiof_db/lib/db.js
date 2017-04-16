44cd82cb60a2ece101ea4f98315aaf0139060be1
yojotaman <yojotaman@gmail.com>
2017-04-15 20:33:51 -0500

    Se cre fixture para test de peticion de imagenes, y se implementa consulta de todas las imagenes de la base de datos

--- studiof_db/lib/db.js
+++ studiof_db/lib/db.js
@@ -46,7 +46,7 @@ class Db {
       let dbTables = yield r.db(db).tableList().run(conn)
       if (dbTables.indexOf('images') === -1) {
         yield r.db(db).tableCreate('images').run(conn)
-        // yield r.db(db).table('images').indexCreate('createdAt').run(conn)
+        yield r.db(db).table('images').indexCreate('createdAt').run(conn)
         // yield r.db(db).table('images').indexCreate('userId', { multi: true }).run(conn)
       }
 
@@ -102,7 +102,78 @@ class Db {
     return Promise.resolve(tasks()).asCallback(callback)
       
   }
+  likeImage (id, callback) {
+    if (!this.connected) {
+      return Promise.reject(new Error('not connected')).asCallback(callback)
+    }
+
+    let connection = this.connection
+    let db = this.db
+    let imageId = uuid.decode(id)
+
+    let tasks = co.wrap(function * () {
+      let conn = yield connection
+
+      let image = yield r.db(db).table('images').get(imageId).run(conn)
+      yield r.db(db).table('images').get(image.id).update({
+        liked: true,
+        likes: image.likes + 1
+      }).run(conn)
+
+      let created = yield r.db(db).table('images').get(imageId).run(conn)
+      return Promise.resolve(created)
+    })
+
+    return Promise.resolve(tasks()).asCallback(callback)
+  }
+
+  getImage (id, callback) {
+    if (!this.connected) {
+      return Promise.reject(new Error('not connected')).asCallback(callback)
+    }
+
+    let connection = this.connection
+    let db = this.db
+    let imageId = uuid.decode(id)
+
+    let tasks = co.wrap(function * () {
+      let conn = yield connection
+      let image = yield r.db(db).table('images').get(imageId).run(conn)
+
+      return Promise.resolve(image)
+    })
+
+    return Promise.resolve(tasks()).asCallback(callback)
+  }
+
+  getImages (callback) {
+    if (!this.connected) {
+      return Promise.reject(new Error('not connected')).asCallback(callback)
+    }
+
+    let connection = this.connection
+    let db = this.db
+
+    let tasks = co.wrap(function * () {
+      let conn = yield connection
+
+      // yield r.db(db).table('images').indexWait().run(conn)
+      let images = yield r.db(db).table('images').orderBy({
+        index: r.desc('createdAt')
+      }).run(conn)
+      let result = yield images.toArray()
+
+      return Promise.resolve(result)
+    })
+
+    return Promise.resolve(tasks()).asCallback(callback)
+  }
+
 }
 
   module.exports = Db
+
+
+
+  
      
--- studiof_db/test/db-test.js
+++ studiof_db/test/db-test.js
@@ -4,37 +4,36 @@ const test = require ('ava')
 const uuid = require('uuid-base62')
 const r = require('rethinkdb')
 const Db = require ('../')
+const fixtures = require('./fixtures')
 
 // configuracion para los test, en este caso se crea una nueva base de datos de prueba para correro los test sobre esta
-const dbName = `studiof_${uuid.v4()}`
-const db = new Db({ db: dbName })
 
-test.before('setup database ', async t => {
+
+test.beforeEach('setup database ', async t => {
+  const dbName = `studiof_${uuid.v4()}`
+  const db = new Db({ db: dbName })
   await db.connect()
+  t.context.db = db
+  t.context.dbName = dbName
   t.true(db.connected, 'should be connected')
 })
 
-test.after('disconnect database', async t => {
+test.afterEach.always('cleanup database', async t => {  
+  let db = t.context.db
+  let dbName = t.context.dbName
   await db.disconnect()
   t.false(db.connected, 'should be disconnected')  
-})
 
-test.after.always('cleanup database', async t => {
   let conn = await r.connect({})
   await r.dbDrop(dbName).run(conn)
 })
 
 
 test('save image', async t => { 
+  let db = t.context.db
   t.is(typeof db.saveImage, 'function', 'saveImage is a function')
 
-  let image = {
-    description: 'an #awesome picture with #tags #studiof',
-    url: `https://studiof.test/${uuid.v4()}.jpg`,
-    likes: 0,
-    liked: false,
-    user_id: uuid.uuid()
-  }
+  let image = fixtures.getImage()
 
   let created = await db.saveImage(image)
   t.is(created.description, image.description)
@@ -46,4 +45,37 @@ test('save image', async t => {
   t.is(typeof created.id, 'string')
   t.is(created.public_id, uuid.encode(created.id))
   t.truthy(created.createdAt)
+})
+
+test('like image', async t => {  
+  let db = t.context.db
+  t.is(typeof db.likeImage, 'function', 'likeImage is a function')
+
+  let image = fixtures.getImage()
+  let created = await db.saveImage(image)
+  let result = await db.likeImage(created.public_id)
+
+  t.true(result.liked)
+  t.is(result.likes, image.likes + 1)
+})
+
+test('get image', async t => {  
+  let db = t.context.db
+  t.is(typeof db.getImage, 'function', 'getImage is a function')
+
+  let image = fixtures.getImage()
+  let created = await db.saveImage(image)
+  let result = await db.getImage(created.public_id)
+
+  t.deepEqual(created, result)  
+})
+
+test('list all images', async t => {
+  let db = t.context.db
+  let images = fixtures.getImages(3)
+  let saveImages = images.map(img => db.saveImage(img))
+  let created = await Promise.all(saveImages)
+  let result = await db.getImages()
+
+  t.is(created.length, result.length)
 })
