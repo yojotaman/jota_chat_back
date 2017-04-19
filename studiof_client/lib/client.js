f74053be1487569e762c6b71bb88d1c61534c06c
yojotaman <yojotaman@gmail.com>
2017-04-19 12:32:58 -0500

    Se crea mock de petición http para obtener imagen

--- studiof_client/lib/client.js
+++ studiof_client/lib/client.js
@@ -1,7 +1,26 @@
 'use strict'
 
+const request = require('request-promise')
+const Promise = require('bluebird')
+
 class Client {
-  getPicture () {}
+  constructor (options) {
+    this.options = options || {
+      endpoints: {
+        pictures: 'http://api.studiofgram.com/picture',
+        users: 'http://api.studiofgram.com/user',
+        auth: 'http://api.studiofgram.com/auth'
+      }
+    }
+  }
+  getPicture (id, callback) {
+    let opts = {
+      method: 'GET',
+      uri: `${this.options.endpoints.pictures}/${id}`,
+      json: true
+    }
+    return Promise.resolve(request(opts)).asCallback(callback)
+  }
 
   savePicture () {}
 
